3d11076df5ca72766417fc36173a35888f00fff4
Ahmed Khanzada <lenzuru@gmail.com>
2019-04-09 12:53:51 -0700

    HTTPS redirect

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const secureRedirect = require('express-http-to-https').redirectToHTTPS
 const app = express()
 const port = process.env.PORT || 1337
 
 app.use(express.static('public'))
+app.use(secureRedirect([/localhost:(\d{4})/], [/\/insecure/], 301));
 app.listen(port, () => console.log(`App listening on port ${port}!`))
