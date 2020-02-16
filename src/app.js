78250f8ecaa5708e2959b4c8fa1a8d156737fd18
Ahmed Khanzada <enzuru@users.noreply.github.com>
2020-02-16 12:13:59 -0800

    Removing backend HTTPS redirect

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require('express')
-const secureRedirect = require('express-http-to-https').redirectToHTTPS
 const app = express()
 const port = process.env.PORT || 1337
 
 app.use(express.static('public'))
-app.use(secureRedirect([/localhost:(\d{4})/], [/\/insecure/], 301))
 app.listen(port, () => console.log(`App listening on port ${port}!`))
